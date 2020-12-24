4c3c69b1116bcc8dc2039d0ceda941de2d97997c
Bern <bernnystrom@gmail.com>
2020-12-25 00:06:35 +0800

    Add game status to change when user presses any key and incrementable game levels everytime nextSequence function is called

--- game.js
+++ game.js
@@ -1,6 +1,8 @@
 const buttonColors = ["red", "blue", "green", "yellow"];
 const gamePattern = [];
 const userClickedPattern = [];
+let gamelevel = 0;
+let gameStarted = false;
 
 const nextSequence = () => {
     // Generates a random number between 0 and 3.
@@ -8,21 +10,18 @@ const nextSequence = () => {
     let randomChosenColor = buttonColors[randomNumber];
     gamePattern.push(randomChosenColor);
 
+    // Increment the game level by 1 everytime the nextSequence() is called.
+    gamelevel++;
+
+    // Updates the h1 level value.
+    $("#level-title").text(`Level ${gamelevel}`);
+
     // Applies a flash effect on the random selected button.
     $(`#${randomChosenColor}`).fadeIn(100).fadeOut(100).fadeIn(100);
 
     // Plays the audio corresponding to the randomly selected button.
     playSound(randomChosenColor);
 }
-
-// User click events for the button.
-$(".btn").on("click", event => {
-    let userChosenColor = event.target.id;
-    userClickedPattern.push(userChosenColor);
-    
-    playSound(userChosenColor); // Plays the audio corresponding to the user selected button.
-    animatePress(userChosenColor); // Applies animation effects to the user selected button.
-});
 
 // Generates and play a new audio corresponding to the randomly selected button.
 const playSound = name => {
@@ -36,4 +35,21 @@ const animatePress = currentColor => {
     setTimeout(() => $(`#${currentColor}`).removeClass("pressed"), 100);
 }
 
-nextSequence();
+// User click events for the button.
+$(".btn").on("click", event => {
+    let userChosenColor = event.target.id;
+    userClickedPattern.push(userChosenColor);
+    
+    playSound(userChosenColor); // Plays the audio corresponding to the user selected button.
+    animatePress(userChosenColor); // Applies animation effects to the user selected button.
+});
+
+// When user presses on any key while on the webpage.
+$(document).on("keydown", () => {
+    // Starts the game.
+    if (!gameStarted) {
+        $("#level-title").text(`Level ${gamelevel}`);
+        nextSequence();
+        gameStarted = true;
+    }
+});
