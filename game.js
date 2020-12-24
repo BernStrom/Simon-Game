9c33755137fcfaf16d8d836c443da575397dfc4c
Bern <bernnystrom@gmail.com>
2020-12-24 23:17:02 +0800

    Add animations to user clicks event and more comments on the code functionality

--- game.js
+++ game.js
@@ -11,20 +11,29 @@ const nextSequence = () => {
     // Applies a flash effect on the random selected button.
     $(`#${randomChosenColor}`).fadeIn(100).fadeOut(100).fadeIn(100);
 
+    // Plays the audio corresponding to the randomly selected button.
     playSound(randomChosenColor);
 }
 
+// User click events for the button.
 $(".btn").on("click", event => {
     let userChosenColor = event.target.id;
     userClickedPattern.push(userChosenColor);
     
-    playSound(userChosenColor);
+    playSound(userChosenColor); // Plays the audio corresponding to the user selected button.
+    animatePress(userChosenColor); // Applies animation effects to the user selected button.
 });
 
 // Generates and play a new audio corresponding to the randomly selected button.
 const playSound = name => {
     let colorAudio = new Audio(`sounds/${name}.mp3`);
     colorAudio.play();
+}
+
+// Animation effects for the button.
+const animatePress = currentColor => {
+    $(currentColor).addClass("pressed");
+    setTimeout(() => $(currentColor).removeClass("pressed"), 100);
 }
 
 nextSequence();
